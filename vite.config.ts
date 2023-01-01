7ff908ce2ce486999b32b1f8c368dd078809e3a7
Sam Estep <sam@samestep.com>
2023-01-01 12:59:31 -0500

    Set `base` in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({ base: "/minkowski/", plugins: [react()] });
